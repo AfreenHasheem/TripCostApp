fd8fbc92fcfb87e83dadcb51009d3f6885dd0650
Siraj <asrf@C02SG255G8WN.local>
2019-06-03 16:16:49 +0100

    Created text boxes and logic to calculate fuel cost and reset the values

--- lib/main.dart
+++ lib/main.dart
@@ -26,8 +26,12 @@ class FuelForm extends StatefulWidget{
 class _FuelFormState extends State<FuelForm>{
   String result = '';
   final _currencies  = ['Dollars', 'Euro', 'Pounds'];
-  String currency = 'Dollars';
+  String _currency = 'Dollars';
+  final double _formDistance = 5.0;
   TextEditingController distanceController = TextEditingController();
+  TextEditingController averageController = TextEditingController();
+  TextEditingController priceController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     TextStyle textStyle = Theme.of(context).textTheme.title;
@@ -40,11 +44,15 @@ class _FuelFormState extends State<FuelForm>{
         padding: EdgeInsets.all(15.0),
         child: Column(
           children: <Widget>[
-            TextField(
+            Padding(
+              padding: EdgeInsets.only(
+                  top: _formDistance ,
+                  bottom: _formDistance),
+            child : TextField(
               controller: distanceController,
               decoration: InputDecoration(
                 labelText: 'Distance',
-                hintText: 'e.g. 124',
+                hintText: 'e.g. 17',
                 labelStyle: textStyle,
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(5.0)
@@ -52,31 +60,98 @@ class _FuelFormState extends State<FuelForm>{
               ),
               keyboardType: TextInputType.number,
 
-            ),
-            DropdownButton<String>(
-                items: _currencies.map((String value){
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-            }).toList(),
-              value: currency,
-              onChanged: (String value) {
-                  _onDropdownChanged(value);
-              },
-            ),
-            RaisedButton(
-              color: Theme.of(context).primaryColorDark,
-              textColor: Theme.of(context).primaryColorLight,
-              onPressed: (){
-                setState(() {
-                  result = distanceController.text;
-                });
-              },
-              child: Text(
-                'Submit',
-                textScaleFactor: 1.5,
+            )),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: _formDistance,
+                  bottom: _formDistance),
+                child : TextField(
+              controller: averageController,
+              decoration: InputDecoration(
+                  labelText: 'Distance per unit',
+                  hintText: 'e.g. 124',
+                  labelStyle: textStyle,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0)
+                  )
               ),
+              keyboardType: TextInputType.number,
+
+            )),
+
+            Padding(
+              padding: EdgeInsets.only(
+                top: _formDistance,
+                bottom: _formDistance
+              ),
+                child :
+                Row(children: <Widget>[
+                  Expanded(
+                    child :   TextField(
+                  controller: priceController,
+                  decoration: InputDecoration(
+                      labelText: 'Price',
+                      hintText: 'e.g. 1.65',
+                      labelStyle: textStyle,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0)
+                      )
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+                ),
+
+                 Container(width: _formDistance * 5),
+
+                 Expanded(
+                     child : DropdownButton<String>(
+                   items: _currencies.map((String value){
+                     return DropdownMenuItem<String>(
+                       value: value,
+                       child: Text(value),
+                     );
+                   }).toList(),
+                   value: _currency,
+                   onChanged: (String value) {
+                     _onDropdownChanged(value);
+                   },
+                 ))
+                ],)
+                ),
+
+            Row(
+              children: <Widget>[
+                Expanded(
+                child : RaisedButton(
+                  color: Theme.of(context).primaryColorDark,
+                  textColor: Theme.of(context).primaryColorLight,
+                  onPressed: (){
+                    setState(() {
+                      result = _calculate();
+                    });
+                  },
+                  child: Text(
+                    'Submit',
+                    textScaleFactor: 1.5,
+                  ),
+                )
+            ),
+
+                Expanded(
+                  child:RaisedButton(
+                    color: Theme.of(context).buttonColor,
+                    textColor: Theme.of(context).primaryColorDark,
+                    onPressed: (){
+                      setState(() {
+                        _reset();
+                      });
+                    },
+                    child: Text(
+                      'Reset',
+                      textScaleFactor: 1.5,
+                    ),
+                  ),)
+              ],
             ),
             Text(result)
           ],
@@ -87,9 +162,28 @@ class _FuelFormState extends State<FuelForm>{
 
    _onDropdownChanged(String value) {
     setState(() {
-      this.currency = value;
+      this._currency = value;
     });
+  }
 
+  String _calculate(){
+    double _distance = double.parse(distanceController.text);
+    double _fuelCost = double.parse(priceController.text);
+    double _consumption = double.parse(averageController.text);
+    double _totalCost = _distance / _consumption * _fuelCost;
+    String _result = 'The total cost of your trip is ' + _totalCost.toStringAsFixed(2) + ' ' + _currency;
+    return _result;
+    
+  }
+
+  void _reset(){
+    distanceController.text = '';
+    priceController.text = '';
+    averageController.text = '';
+    setState(() {
+      result = '';
+    });
   }
 }
+
 
